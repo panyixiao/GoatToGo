25e609cc52183f99ad57aa5ab34de74615bdbb63
nsmahajan <nsmahajan@wpi.edu>
2015-10-28 18:55:00 -0400

    New application window size
    Changed the login and welcome page with new application window size.

--- src/gtg_view_subsystem/Page.java
+++ src/gtg_view_subsystem/Page.java
@@ -9,13 +9,17 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class Page extends JFrame {
-	private JPanel mainPanel, headerPanel;
+	private JPanel mainPanel, headerPanel, dragpanel;
 	private JLabel wpiLogoHolder, lblGoattogo;
 	private ImageIcon wpiLogoImage, minimizeBtnImage, closeBtnImage;
 	private JButton minimizeBtn, closeBtn, adminBtn, logoutBtn;
 	private MainView parent;
+	private int pX, pY;
 	/**
 	 * Create the frame.
 	 * @param mainView 
@@ -23,12 +27,13 @@ public class Page extends JFrame {
 	public Page(MainView mainView) {
 		this.parent = mainView;
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(500, 200, 1057, 601);
+		this.setBounds(100, 100, 1366, 728);
+		//this.setBounds(500, 200, 1057, 601);
 		this.setUndecorated(true);
 		this.getContentPane().setLayout(null);
 		
 		this.headerPanel = new JPanel();
-		this.headerPanel.setBounds(0, 0, 1057, 67);
+		this.headerPanel.setBounds(0, 0, 1366, 67);
 		this.headerPanel.setBackground(new Color(0xc30e2d));
 		this.headerPanel.setLayout(null);
 		this.getContentPane().add(this.headerPanel);
@@ -51,7 +56,7 @@ public class Page extends JFrame {
 				setState(Frame.ICONIFIED);
 			}
 		});
-		this.minimizeBtn.setBounds(975, 45, 22, 4);
+		this.minimizeBtn.setBounds(1285, 45, 22, 4);
 		this.minimizeBtn.setContentAreaFilled(false);
 		this.minimizeBtn.setBorder(null);
 		this.minimizeBtnImage = new ImageIcon(ImageURLS.MINIMIZE_BUTTON);
@@ -64,7 +69,7 @@ public class Page extends JFrame {
 				System.exit(0);
 			}
 		});
-		this.closeBtn.setBounds(1012, 22, 29, 27);
+		this.closeBtn.setBounds(1322, 22, 29, 27);
 		this.closeBtn.setContentAreaFilled(false);
 		this.closeBtn.setBorder(null);
 		this.closeBtnImage = new ImageIcon(ImageURLS.CLOSE_BUTTON);
@@ -81,7 +86,7 @@ public class Page extends JFrame {
 		this.adminBtn.setFont(new Font("Meiryo", Font.PLAIN, 20));
 		this.adminBtn.setBackground(null);
 		this.adminBtn.setForeground(Color.WHITE);
-		this.adminBtn.setBounds(870, 18, 70, 44);
+		this.adminBtn.setBounds(1180, 18, 70, 44);
 		this.adminBtn.setBorder(null);
 		this.adminBtn.setFocusPainted(false);
 		headerPanel.add(this.adminBtn);
@@ -96,15 +101,33 @@ public class Page extends JFrame {
 		this.logoutBtn.setFont(new Font("Meiryo", Font.PLAIN, 20));
 		this.logoutBtn.setBackground(null);
 		this.logoutBtn.setForeground(Color.WHITE);
-		this.logoutBtn.setBounds(870, 18, 70, 44);
+		this.logoutBtn.setBounds(1180, 18, 70, 44);
 		this.logoutBtn.setBorder(null);
 		this.logoutBtn.setFocusPainted(false);
 		this.logoutBtn.setVisible(false);
 		headerPanel.add(this.logoutBtn);
+		
+		this.dragpanel = new JPanel();
+		this.dragpanel.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent mouseEvent) {
+				setLocation(getLocation().x+mouseEvent.getX()-pX,getLocation().y+mouseEvent.getY()-pY);
+			}
+		});
+		this.dragpanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent mouseEvent) {
+				pX=mouseEvent.getX();
+                pY=mouseEvent.getY();
+			}
+		});
+		this.dragpanel.setBounds(0, 0, 1178, 67);
+		this.dragpanel.setBackground(null);
+		headerPanel.add(this.dragpanel);
 
 		this.mainPanel = new JPanel();
 		mainPanel.setBackground(Color.WHITE);
-		this.mainPanel.setBounds(0, 67, 1057, 534);
+		this.mainPanel.setBounds(0, 67, 1366, 661);
 		this.getContentPane().add(mainPanel);
 
 		this.setVisible(true);
