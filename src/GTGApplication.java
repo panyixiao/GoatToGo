e93e3ffdf3819123cbbf93c42f208acf8ae9c0c5
panyixiao <panyixiao@126.com>
2015-11-01 20:19:40 -0500

    3 facade created

--- src/GTGApplication.java
+++ src/GTGApplication.java
@@ -1,10 +1,12 @@
 import gtg_view_subsystem.MainView;
-import gtg_control_subsystem.MainController;;
+import gtg_control_subsystem.MainController;
+import gtg_model_subsystem.MainModel;
 
 public class GTGApplication {
 	public static void main(String[] args) {
 		MainView mainView= new MainView();
 		MainController mainController = new MainController();
+		MainModel mainModel = new MainModel();
 	}
 
 }
