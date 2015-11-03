59a8f6d127c5687e6dae52ef150a4b40c5f3fcab
Joshua T Pritchett <jpritchett757@gmail.com>
2015-11-03 00:21:10 -0500

    check changes model log.txt

---
+++ src/gtg_model_subsystem/FileProcessing.java
@@ -0,0 +1,103 @@
+package gtg_model_subsystem;
+
+import gtg_model_subsystem.Node;
+import gtg_model_subsystem.Edge;
+import gtg_model_subsystem.Admin;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.List;
+
+public class FileProcessing {
+	
+	public void readNodesFile(List<Node> nodes, String mapNodeURL) throws IOException{
+		File file = new File(mapNodeURL);
+		BufferedReader buffer = new BufferedReader(new FileReader(file));
+		String line;
+		String[] lines;
+		while((line = buffer.readLine()) != null){
+			line = line.trim();
+			lines = line.split("[\\s+]");
+			
+			Node node = new Node(Integer.parseInt(lines[0]), 
+								 Integer.parseInt(lines[1]), 
+								 Integer.parseInt(lines[2])
+								 );
+			nodes.add(node);
+		}
+	}
+	public void readEdgesFile(List<Node> nodes, List<Edge> edges, String mapEdgeURL) throws IOException{
+		File file = new File(mapEdgeURL);
+		BufferedReader buffer = new BufferedReader(new FileReader(file));
+		String line;
+		String[] lines;
+		int nodeId1, nodeId2;
+		while((line = buffer.readLine()) != null){
+			line = line.trim();
+			lines = line.split("[\\s+]");
+			
+			
+			nodeId1 = Integer.parseInt(lines[1]);
+			nodeId2 = Integer.parseInt(lines[2]);
+
+			Edge edge = new Edge(Integer.parseInt(lines[0]), 
+								 nodes.get(nodeId1 - 1), 
+								 nodes.get(nodeId2 - 1),
+								 calculateDistance(nodes.get(nodeId1).getX(), nodes.get(nodeId2).getX(), nodes.get(nodeId1).getY(), nodes.get(nodeId2).getY())
+								 );
+			edges.add(edge);
+		}
+	}
+	public void readAdmin(List<Admin> admins, String adminURL) throws IOException{
+		File file = new File(adminURL);
+		BufferedReader buffer = new BufferedReader(new FileReader(file));
+		String line;
+		String[] lines;
+		int nodeId1, nodeId2;
+		while((line = buffer.readLine()) != null){
+				line = line.trim();
+				lines = line.split("[\\s+]");
+				
+				Admin admin = new Admin(lines[0], lines[1]);
+				admins.add(admin);
+		}
+	}
+	public void saveNodesFile(List<Node> nodes, String mapNodeURL) throws IOException{
+		try{
+		    FileWriter fstream = new FileWriter(mapNodeURL);
+		    BufferedWriter out = new BufferedWriter(fstream);
+		    for(Node node: nodes){
+		    	out.write(node.getID() + " " + node.getX()+ " " + node.getY());
+		    	out.newLine();
+		    }
+		    System.out.println("File Node Write Success!");
+		    out.close();
+		}
+		catch(Exception E){
+			System.out.println(E.toString());
+		}
+	}
+	public void saveEdgesFile(List<Edge> edges, String mapEdgeURL) throws IOException{
+		try{
+		    FileWriter fstream = new FileWriter(mapEdgeURL);
+		    BufferedWriter out = new BufferedWriter(fstream);
+		    for(Edge edge: edges){
+		    	out.write(edge.getEdgeID() + " " + edge.getSource().getID() + " " + edge.getDestination().getID());
+		    	out.newLine();
+		    }
+		    System.out.println("File Edge Write Success!");
+		    out.close();
+		}
+		catch(Exception E){
+			System.out.println(E.toString());
+		}
+	}
+	private double calculateDistance(double x1, double x2, double y1, double y2)
+	{
+		return Math.sqrt(Math.pow(x2-x1, 2)+ Math.pow(y2 - y1, 2));
+	}
+	
+}
--- src/gtg_model_subsystem/TestingDijkstra.java
+++ src/gtg_model_subsystem/TestingDijkstra.java
@@ -3,56 +3,92 @@ package gtg_model_subsystem;
 import gtg_model_subsystem.Node;
 import gtg_model_subsystem.CoordinateGraph;
 import gtg_model_subsystem.Edge;
-
+import gtg_model_subsystem.ShortestPath;
 import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class TestingDijkstra {
 	private List<Node> nodes;
 	private List<Edge> edges;
-	private FileLoader fileLoader;
+	private LinkedList<Node> path;
+	private CoordinateGraph graph;
+	private List<Admin> admins;
+	private FileProcessing fileProcessing;
+	
 	public void testDij(){
 		
 		edges = new ArrayList<Edge>();
 		nodes = new ArrayList<Node>();
-		fileLoader = new FileLoader();
+		admins = new ArrayList<Admin>();
+		fileProcessing = new FileProcessing();
+		//testing for loading admins
+		try{
+			loadAdmin();
+			printAdmins();
+		}
+		catch(IOException e){
+			System.out.println(e.toString());
+		}
+		//testing for loading of nodes/edges
 		try {
-			LoadNodes();
-			LoadEdges();
+			loadNodes();
+			loadEdges();
+			graph = new CoordinateGraph(nodes, edges);
+			
 			printNodes();
 			printEdges();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e.toString());
+		}//END CATCH loadNodes/Edges
+		
+		//testing for saving nodes and edges
+		try {
+			saveNodes();
+			saveEdges();
 		}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.toString());
+		}//END CATCH saveNodes/Edges
+	}
+	void loadAdmin() throws IOException{
+		fileProcessing.readAdmin(admins, "ModelFiles\\adminFile.txt");
+	}
+	void loadNodes() throws IOException{
+		fileProcessing.readNodesFile(nodes, MapNodeURLS.TEST_MAP_NODES);
 		
 	}
-	void LoadNodes() throws IOException{
-		fileLoader.readNodesFile(nodes, MapNodeURLS.TEST_MAP_NODES);
-		
+	void loadEdges() throws IOException{
+		fileProcessing.readEdgesFile(nodes, edges, MapEdgeURLS.TEST_MAP_EDGES);
 	}
-	void LoadEdges() throws IOException{
-		fileLoader.readEdgesFile(nodes, edges, MapEdgeURLS.TEST_MAP_EDGES);
+	void saveNodes() throws IOException{
+		fileProcessing.saveNodesFile(nodes, MapNodeURLS.TEST_MAP_NODES);
 	}
-	
+	void saveEdges() throws IOException{
+		fileProcessing.saveEdgesFile(edges, MapEdgeURLS.TEST_MAP_EDGES);
+	}
 	void printNodes(){
 		for(Node node: nodes){
-			System.out.print(node.getID() + " ");
-			System.out.print(node.getX() + " ");
-			System.out.print(node.getY() + " ");
+			System.out.print(node.getID() + " " + node.getX() + " " + node.getY());
 			System.out.println();
 		}
 		System.out.println("END PRINT NODES");
 	}
 	void printEdges(){
 		for(Edge edge: edges){
-			System.out.print(edge.getEdgeID() + " ");
-			System.out.print(edge.getSource().getID() + " ");
-			System.out.print(edge.getDestination().getID() + " ");
-			System.out.print(edge.getEdgeLength() + " ");
+			System.out.print(edge.getEdgeID() + " " + edge.getSource().getID() + " " + edge.getDestination().getID() + " " + edge.getEdgeLength());
 			System.out.println();
 		}
 		System.out.println("END PRINT EDGES");
+	}
+	void printAdmins(){
+		for(Admin admin: admins){
+			System.out.print(admin.getUsername()+ " " + admin.getPassword());
+			System.out.println();
+		}
+		System.out.println("END OF PRINT ADMIN");
 	}
 }
