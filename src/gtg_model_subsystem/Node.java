bf509713d87588af668e2a860e86f74ff97b0242
zhangjingjun <jingjunzhang@Jingjuns-MacBook-Air.local>
2015-11-18 23:03:15 -0500

    Complete Node class with all attributes and compatible construction.
    Complete Node class with all attributes and compatible construction.

--- src/gtg_model_subsystem/Node.java
+++ src/gtg_model_subsystem/Node.java
@@ -13,8 +13,12 @@ public class Node
 		 */
 		private int  y;
 		
+		private int floorNum;
+		// floor number, 1 by default
 		private int entranceID;
-		// entrance ID, 0 means it is not an entrance
+		// entrance ID, 0 by default, means it is not an entrance
+		private String buildingName;
+		// building name
 		private String nodeType;
 		// type of node, for example, classroom, staircase or restroom.
 		private String nodeDescription;
@@ -24,21 +28,22 @@ public class Node
 		 */
 		//private NodeAttribute attribute;
 		
-		public Node(int id, int x, int y, int entranceID, String nodeType, String nodeDescription) {
+		public Node(int id, int x, int y, int floorNum, int entranceID, String buildingName, String nodeType, String nodeDescription) {
 			this.id = id;
 			this.x = x;
 			this.y = y;
+			this.floorNum=floorNum;
 			this.entranceID=entranceID;
+			this.buildingName=buildingName;
 			this.nodeType=nodeType;
 			this.nodeDescription=nodeDescription;
 		}
 		// construction
+		
 		public Node(int id, int x, int y) {
-			this.id = id;
-			this.x = x;
-			this.y = y;
+			this(id, x, y, 1, 0, "null", "null", "null");
 		}
-		// overload construction
+		// overload construction, compatible with current code
 		
 		public int getID(){
 			return this.id;
@@ -51,12 +56,22 @@ public class Node
 		public int getY(){
 			return this.y;
 		}
+		public int getFloorNum(){
+			return this.floorNum;
+		}
+		
 		public int getEntranceID(){
 			return this.entranceID;
 		}
+		
+		public String getBuildingName(){
+			return this.buildingName;
+		}
+		
 		public String getNodeType(){
 			return this.nodeType;
 		}
+		
 		public String getNodeDescription(){
 			return this.nodeDescription;
 		}
